b34b678c522e923aa0f0f9c54eedced53d9a24d7
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-01 19:28:41 -0500

    removed Security and login

---
+++ src/main/java/com/websummarizer/Web/Summarizer/configs/CustomUserService.java
@@ -0,0 +1,26 @@
+//package com.websummarizer.Web.Summarizer.configs;
+//
+//import com.websummarizer.Web.Summarizer.model.User;
+//import com.websummarizer.Web.Summarizer.repo.UserRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserService implements UserDetailsService {
+//    @Autowired
+//    private UserRepo userRepo;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        User user = userRepo.findByEmail(email);
+//        if(user!=null){
+//            return new CustomUser(user);
+//        }
+//
+//        throw new UsernameNotFoundException("User not available");
+//    }
+//}
--- src/main/java/com/websummarizer/Web/Summarizer/services/UserServiceImpl.java
+++ src/main/java/com/websummarizer/Web/Summarizer/services/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.websummarizer.Web.Summarizer.services;
 import com.websummarizer.Web.Summarizer.model.User;
 import com.websummarizer.Web.Summarizer.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +11,12 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepo userRepo;
-
+//    @Autowired
+//    private BCryptPasswordEncoder passwordEncoder;
     @Override
     public User createUser(User user) {
+        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        //user.setRole("ROLE_USER");
         return userRepo.save(user);
     }
 
