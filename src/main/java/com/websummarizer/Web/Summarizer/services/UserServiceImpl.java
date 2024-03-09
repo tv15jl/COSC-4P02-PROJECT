3bba48687226bd6e7fc02b77be84d12713bdf2b2
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-09 13:07:13 -0500

    Added encryption to password saved in DB

--- src/main/java/com/websummarizer/Web/Summarizer/services/UserServiceImpl.java
+++ src/main/java/com/websummarizer/Web/Summarizer/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.websummarizer.Web.Summarizer.model.User;
 import com.websummarizer.Web.Summarizer.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,12 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepo userRepo;
-//    @Autowired
-//    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     @Override
     public User createUser(User user) {
-        //user.setPassword(passwordEncoder.encode(user.getPassword()));
-        //user.setRole("ROLE_USER");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_USER");
         return userRepo.save(user);
     }
 
